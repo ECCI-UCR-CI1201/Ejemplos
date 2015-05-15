f4507ef755603c01822d894dc06bb163ef0073e0
Rubén Jiménez Goñi <rubenjg1991@gmail.com>
2015-05-15 17:56:43 -0600

    Se agrega Ejemplo Factorial

---
+++ EjemploFactorial/EjemploFactorial/EjemploFactorial.cpp
@@ -0,0 +1,25 @@
+// EjemploFactorial.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int _tmain(int argc, _TCHAR* argv[]){
+
+	cout << 4 << "! es " << factorial(4) << endl;
+	cout << 5 << "! es " << factorial(5) << endl;
+	cout << 6 << "! es " << factorial(6) << endl;
+
+	return 0;
+}
+
+int factorial(int n) {
+	if (n > 1) {
+		n *= factorial(n - 1);
+	}
+	return n;
+}

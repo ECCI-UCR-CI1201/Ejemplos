edb6e7462ef4fabd812bc3c6f623f6b0b57b6f2f
Rubén Jiménez Goñi <rubenjg1991@gmail.com>
2015-05-20 16:33:29 -0600

    Se agrega Ejemplo Punteros

--- EjemploPunteros/EjemploPunteros.cpp
+++ EjemploPunteros/EjemploPunteros.cpp
@@ -1,11 +1,35 @@
-// EjemploPunteros.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	int * v = new int;
+
+	*v = 5;
+
+	cout << "Direccion de memoria v " << v << endl;
+	cout << "A lo que apunta v " << *v << endl;
+
+	int ** p = &v;
+
+	cout << "Direccion de memoria p " << p << endl;
+	cout << "Direccion de memoria v " << *p << endl;
+	cout << "A lo que apunta v " << **p << endl;
+
+	**p = 7;
+	cout << "A lo que apunta v " << *v << endl;
+
+	delete v;
+
+	int x = -5;
+	char * c = (char *) &x;
+
+	for (int i = 0; i < sizeof(x); ++i) {
+		cout << i << " " << (int) c[i] << endl;
+	}
+
+	system("pause");
 	return 0;
 }
-

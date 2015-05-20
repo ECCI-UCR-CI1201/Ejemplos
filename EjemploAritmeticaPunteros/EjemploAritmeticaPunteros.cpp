67b75d1a3f1d7fe9f747b52e0fc0d3b376893e42
Rubén Jiménez Goñi <rubenjg1991@gmail.com>
2015-05-20 16:40:24 -0600

    Se agrega Ejemplo Aritmética Punteros

--- EjemploAritmeticaPunteros/EjemploAritmeticaPunteros.cpp
+++ EjemploAritmeticaPunteros/EjemploAritmeticaPunteros.cpp
@@ -1,11 +1,43 @@
-// EjemploAritmeticaPunteros.cpp : Defines the entry point for the console application.
-//
-
+ 
 #include "stdafx.h"
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	int n = 10;
+
+	// Creación de un vector
+	int * p = new int[n];
+	for (int i = 0; i < n; ++i) {
+		p[i] = i + 1;
+	}
+
+	// Recorrido del vector
+	for (int i = 0; i < n; ++i) {
+		cout << p[i] << " ";
+	}
+	cout << endl;
+
+	// Recorrido usando aritmetica de punteros
+	for (int * i = p; i < p + n; i++) {
+		(*i)++;
+		cout << *i << " ";
+	}
+	cout << endl;
+
+	// Algo oscuro
+	int v = 256 * 256 - 1;
+	char * perro = (char *) &v; // Sugerido por David
+	cout << "V original: " << v << endl;
+	for (char * i = perro; i < perro + 4; ++i) {
+		cout << (int) ((unsigned char) *i) << " "; // Muy oscuro
+		(*i)++; // Algo más oscuro
+	}
+	cout << endl;
+	cout << "V modificado: " << v << endl;
+
+	system("pause");
 	return 0;
 }
 

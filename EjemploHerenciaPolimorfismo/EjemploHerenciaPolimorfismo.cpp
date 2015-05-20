553f901f50648df96329334c112aad267e8aac36
Rubén Jiménez Goñi <rubenjg1991@gmail.com>
2015-05-20 17:58:31 -0600

    Se agrega Ejemplo Herencia Polimorfismo

--- EjemploHerenciaPolimorfismo/EjemploHerenciaPolimorfismo.cpp
+++ EjemploHerenciaPolimorfismo/EjemploHerenciaPolimorfismo.cpp
@@ -1,11 +1,26 @@
-// EjemploHerenciaPolimorfismo.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
+#include "Elemento.h"
+#include "ElementoInt.h"
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	ElementoInt a(10);
+	ElementoInt b(2);
+	ElementoInt c(3);
+
+	cout << "A: " << a << endl;
+	cout << "B: " << b << endl;
+	cout << "C: " << c << endl;
+
+	if (a < b) {
+		cout << "A es menor que B" << endl;
+	} else {
+		cout << "B es menor que A" << endl;
+	}
+
+	system("pause");
 	return 0;
 }
-

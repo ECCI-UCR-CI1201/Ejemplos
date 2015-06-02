c15ad8a684a2000be1bf9512da80bd1ea46ed0a5
Rubén Jiménez Goñi <rubenjg1991@gmail.com>
2015-06-02 14:54:14 -0600

    Se agrega EjemploPlantillas
    - Ejemplifica el uso de clases emplantilladas y de funciones con
    plantillas.

---
+++ EjemploPlantillas/EjemploPlantillas.cpp
@@ -0,0 +1,50 @@
+// EjemploPlantillas.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Matriz.h"
+
+template<class T>
+void imprimirMatriz();
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	srand((unsigned int) time(NULL));
+
+	imprimirMatriz<int>();
+	imprimirMatriz<double>();
+	imprimirMatriz<char>();
+
+	system("pause");
+	return 0;
+}
+
+template<class T>
+void imprimirMatriz() {
+	Matriz<T> m(5, 5);
+	for (int i = 0; i < m.getFilas(); i++) {
+		for (int j = 0; j < m.getColumnas(); j++) {
+			m.set(i, j, random<T>());
+		}
+	}
+	cout << m << endl;
+}
+
+template<class T>
+T random() {
+	return rand() % 100;
+}
+
+// Especializacion de template para char
+template<>
+char random<char>() {
+	return rand() % 26 + 'a';
+}
+
+// Especializacion de template para double
+template<>
+double random<double>() {
+	return (double) (rand() % 200) / 2.0;
+}
+
+
+
---
+++ EjemploPlantillas/Matriz.h
@@ -0,0 +1,65 @@
+
+#pragma once
+
+#include "stdafx.h"
+using namespace std;
+
+template<class T>
+class Matriz {
+
+	template<class T>
+	friend ostream& operator<<(ostream&, Matriz<T>& e);
+
+private:
+	T ** matriz;
+	int n, m;
+
+public:
+
+	Matriz(int n, int m) {
+		this->n = n;
+		this->m = n;
+		this->matriz = new T*[n];
+		for (int i = 0; i < n; i++) {
+			this->matriz[i] = new T[m];
+			for (int j = 0; j < m; j++) {
+				this->matriz[i][j] = 0;
+			}
+		}
+	}
+
+	~Matriz() {
+		for (int i = n - 1; i >= 0; i--) {
+			delete this->matriz[i];
+		}
+		delete this->matriz;
+	}
+
+	int getFilas() {
+		return n;
+	}
+
+	int getColumnas() {
+		return m;
+	}
+
+	void set(int i, int j, T e) {
+		this->matriz[i][j] = e;
+	}
+
+	T get(int i, int j) {
+		return this->matriz[i][j];
+	}
+};
+
+template<class T> 
+ostream& operator<<(ostream & out, Matriz<T>& m) {
+	for (int i = 0; i < m.n; i++) {
+		for (int j = 0; j < m.m; j++) {
+			out << m.matriz[i][j] << "\t";
+		}
+		out << endl;
+	}
+	return out;
+}
+
